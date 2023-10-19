668a125e50fc47b8c9f79a6bfd8b76de3ae1bb99
DoctorSager <doctorsager163@gmail.com>
2023-10-19 15:25:56 +0300

    [First]

---
+++ Number Guessing Game/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Number_Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtEnterNumber = new System.Windows.Forms.TextBox();
+            this.lbmQuetion = new System.Windows.Forms.Label();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(328, 283);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(160, 65);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Проверка";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.CheckNumber);
+            // 
+            // txtEnterNumber
+            // 
+            this.txtEnterNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtEnterNumber.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnterNumber.Location = new System.Drawing.Point(210, 173);
+            this.txtEnterNumber.Name = "txtEnterNumber";
+            this.txtEnterNumber.Size = new System.Drawing.Size(377, 41);
+            this.txtEnterNumber.TabIndex = 1;
+            this.txtEnterNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbmQuetion
+            // 
+            this.lbmQuetion.AutoSize = true;
+            this.lbmQuetion.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbmQuetion.Location = new System.Drawing.Point(228, 132);
+            this.lbmQuetion.Name = "lbmQuetion";
+            this.lbmQuetion.Size = new System.Drawing.Size(350, 28);
+            this.lbmQuetion.TabIndex = 2;
+            this.lbmQuetion.Text = "Я загадал число от 0 до 100";
+            this.lbmQuetion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.Location = new System.Drawing.Point(229, 231);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(219, 21);
+            this.lblGuess.TabIndex = 3;
+            this.lblGuess.Text = "Количество попыток: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.lbmQuetion);
+            this.Controls.Add(this.txtEnterNumber);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+        private System.Windows.Forms.Label lbmQuetion;
+        private System.Windows.Forms.Label lblGuess;
+    }
+}
+
---
+++ Number Guessing Game/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+    
+
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        int number = 0;
+        int guess = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadQuestions();
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(txtEnterNumber.Text);
+
+            guess += 1;
+            lblGuess.Text = $"Количество попыток: {guess}";
+
+            if (i == number)
+            {
+                MessageBox.Show("Круто! Ты угадал число. Попробуй еще");
+                LoadQuestions();
+                txtEnterNumber.Text = "";
+                guess = 0;
+                lblGuess.Text = $"Количество попыток: {guess}";
+            }
+            else if (i > number)
+            {
+                MessageBox.Show("Слишком большое!");
+                
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Слишком маленькое!");
+                
+            }
+
+        }
+
+        private void LoadQuestions()
+        {
+            number = random.Next(0, 100);
+
+            lbmQuetion.Text = " Я загадал число между 0 и 100 ";
+        }
+
+
+    }
+}
